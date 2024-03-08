2d15e4477eaccb8c743ffab4403446008e392fc6
NataliaLM-26 <t.natalialm@gmail.com>
2024-03-07 19:42:39 -0600

    conecta componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'panel', component: MainComponent},
+];
+
+export const appRoutingProviders: any[]=[];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
