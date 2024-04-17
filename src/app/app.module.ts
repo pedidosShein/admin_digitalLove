27abd4b53334c61706ab0ab75c38fb54e6fc9b17
NataliaLM-26 <t.natalialm@gmail.com>
2024-04-17 07:51:03 -0600

    crea componentes: notificaciones, usuariosy perfil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
 import { NavComponent } from './nav/nav.component';
+import { NotificacioesComponent } from './notificacioes/notificacioes.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     MainComponent,
-    NavComponent
+    NavComponent,
+    NotificacioesComponent,
+    UsuariosComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
