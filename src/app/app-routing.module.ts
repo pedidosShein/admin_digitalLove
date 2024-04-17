378167418c21dac2cb1aae227ef9175287cf8b1d
NataliaLM-26 <t.natalialm@gmail.com>
2024-04-17 08:02:17 -0600

    configuración de nuevos módulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
+import { NotificacioesComponent } from './notificacioes/notificacioes.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+
 import { NavComponent } from './nav/nav.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'panel', component: MainComponent},
   {path: 'nav', component: NavComponent},
+  { path: 'notificaciones', component: NotificacioesComponent},
+  { path: 'perfil', component:  PerfilComponent},
+  { path: 'usuarios', component:UsuariosComponent}
 ];
 
 export const appRoutingProviders: any[]=[];
