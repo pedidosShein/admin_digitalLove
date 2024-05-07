e01dcefcec5ea168a995af35569438dba8786cf0
NataliaLM-26 <t.natalialm@gmail.com>
2024-05-07 09:36:47 -0600

    usuarios con token

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.model';
 
@@ -7,13 +7,17 @@ import { User } from '../models/user.model';
   providedIn: 'root'
 })
 export class UsersService {
-  private apiUrl = 'http://frantic-livvyy-jazael.koyeb.app/api/v1/usuarios/'
+  private apiUrl = 'https://gigantic-mora-jazael-3245dd16.koyeb.app/api/v1/usuariosApp/';
+  private token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE3NjM1NzUyLCJpYXQiOjE3MTUwNDM3NTIsImp0aSI6IjZiNmNhYjk1NTlmNTRiMDFiZWU1MWEyYjVhMDY3NjdiIiwidXNlcl9pZCI6Mn0.NqvfTuAY4OL9ZsYhXCiYchbvXZ8_d1DoBdLnFLWZz1o';
 
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.apiUrl);
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer ' + this.token
+    });
+
+    
+    return this.http.get<User[]>(this.apiUrl, { headers });
   }
-
-
 }
