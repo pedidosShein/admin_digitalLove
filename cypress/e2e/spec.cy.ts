599053bc71c8c5d859ba4e0645d04c5f5b8c7c09
NataliaLM-26 <t.natalialm@gmail.com>
2024-05-20 09:08:20 -0600

    primeras pruebas e2e

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,74 @@
-describe('My First Test', () => {
+describe('Login module', () => {
   it('Visits the initial project page', () => {
     cy.visit('/')
-  })
+  });
+
+  it('Should exist input to login', () => {
+    cy.visit('/');
+    cy.get('#usuario').should ('exist');
+    cy.get('#contrasena').should ('exist');
+  });
+
+  it('Should enter valid email and password and redirect to the main', () => {
+    cy.visit('/');
+    cy.get('#loginForm', { timeout: 10000 }).should('exist');
+    cy.get('#usuario').type('admin');
+    cy.get('#contrasena').type('admin');
+    cy.get('#btn').click();
+    cy.visit('/panel');
+  });
+
 });
 
+describe('Navbar module', () => {
+  it('Visits all pages', () => {
+    cy.visit('/panel');
+    cy.wait(1000);
+    cy.visit('/notificaciones');
+    cy.wait(1000);
+    cy.visit('/usuarios');
+    cy.wait(1000);
+    cy.visit('/perfil');
+    cy.wait(1000);
+    cy.visit('/');
+    cy.wait(1000);
+  });
+
+
+});
+
+describe('Panel module', () => {
+  it('Visits main page', () => {
+    cy.visit('/panel');
+    cy.get('#titulo').should ('be.visible');
+    cy.get('#titulo').should ('have.text', 'Mapa');
+  });
+
+});
+
+describe('Notificaciones module', () => {
+  it('Visits notifications page', () => {
+    cy.visit('/notificaciones');
+    cy.get('#titulo').should ('be.visible');
+    cy.get('#titulo').should ('have.text', 'Notificaciones');
+  });
+
+});
+
+describe('Usuarios module', () => {
+  it('Visits users page', () => {
+    cy.visit('/usuarios');
+    cy.get('#titulo').should ('be.visible');
+    cy.get('#titulo').should ('have.text', 'Usuarios');
+  });
+
+});
+
+describe('Perfil module', () => {
+  it('Visits profile page', () => {
+    cy.visit('/perfil');
+    cy.get('#titulo').should ('be.visible');
+    cy.get('#titulo').should ('have.text', 'Perfil');
+  });
+
+});
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,20 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
 import { NotificacioesComponent } from './notificacioes/notificacioes.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
-
 import { NavComponent } from './nav/nav.component';
-import { MapComponent } from './map/map.component';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent},
-  {path: 'main', component: MainComponent},
-  {path: 'panel', component: MainComponent},
-  {path: 'nav', component: NavComponent},
-  { path: 'notificaciones', component: NotificacioesComponent},
-  { path: 'perfil', component:  PerfilComponent},
-  { path: 'usuarios', component:UsuariosComponent}
+  { path : '', component: LoginComponent },
+  { path : 'panel', component: MainComponent },
+  { path : 'nav', component: NavComponent },
+  { path : 'notificaciones', component: NotificacioesComponent },
+  { path : 'perfil', component:  PerfilComponent },
+  { path : 'usuarios', component:UsuariosComponent }
 ];
 
 export const appRoutingProviders: any[]=[];
