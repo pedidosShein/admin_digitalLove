ab53d83938ef4e91aa307310a37b581b2100a748
NataliaLM-26 <t.natalialm@gmail.com>
2024-05-23 15:33:41 -0600

    cambios

--- src/app/notificaciones/notificaciones.component.ts
+++ src/app/notificaciones/notificaciones.component.ts
@@ -21,6 +21,23 @@ export class NotificacioesComponent implements OnInit {
       (error) => {
         console.log('Error al obtener los usuarios:', error);
       });
+  }
+
+  formatDate(date: Date): string {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Los meses comienzan desde 0
+    const year = date.getFullYear();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
     
+    return `${day}-${month}-${year} ${hours}:${minutes}`;
+}
+
+  
+
+  getDate(){
+    const fecha = new Date('2024-05-21T03:17:20.036090Z');
+    const formattedDate = this.formatDate(fecha);
+    console.log(formattedDate);
   }
 }
--- src/app/services/notificaciones.service.ts
+++ src/app/services/notificaciones.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class NotificacionesService {
-  private apiUrl = 'https://gigantic-mora-jazael-3245dd16.koyeb.app/api/v1/usuariosApp/';
+  private apiUrl = 'https://gigantic-mora-jazael-3245dd16.koyeb.app/api/v1/reportes/';
   private token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE3NjM1NzUyLCJpYXQiOjE3MTUwNDM3NTIsImp0aSI6IjZiNmNhYjk1NTlmNTRiMDFiZWU1MWEyYjVhMDY3NjdiIiwidXNlcl9pZCI6Mn0.NqvfTuAY4OL9ZsYhXCiYchbvXZ8_d1DoBdLnFLWZz1o';
 
   constructor(private http: HttpClient) { }
