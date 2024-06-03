ed63f4110036e04c8ba37a73ae11212daf4782aa
NataliaLM-26 <t.natalialm@gmail.com>
2024-06-03 09:15:52 -0600

    contraseña formateada

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
@@ -9,12 +8,19 @@ import { Component, OnInit } from '@angular/core';
 
 export class PerfilComponent implements OnInit{
   usuario: string | null = '';
-  password: string | null = '';
+  password!: string | null;
+  passwordOculta!: string;
 
   ngOnInit(): void {
       this.usuario = localStorage.getItem('usuario');
       this.password = localStorage.getItem('password');
+      this.passwordOculta = this.ocultarContraseña(this.password);
   }
   
-  
+  ocultarContraseña(contraseña: string | null): string {
+    if (contraseña === null) {
+      return '';
+    }
+    return '*'.repeat(contraseña.length);
+  }
 }
